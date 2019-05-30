d240e4eb81108756686f8643d43a0126c9a2e285
anjanianusuri <38336093+anjanianusuri@users.noreply.github.com>
2019-05-30 11:16:18 +1000

    sports

---
+++ sports-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Sports Plugin
+ * Plugin URI: http://localhost/wordpress
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Anjani Anusuri
+ * Author URI: http://localhost/wordpress
+ */
